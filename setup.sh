cbaddbcfeb572e01d82c4aeeb63ddc21c238f2ab
Phạm Thành Tuyên <149477072+phm-tuyenn@users.noreply.github.com>
2024-01-27 16:42:29 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-apt install gnupg ca-certificates -y
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-apt update
-apt install nodejs -y
+
 apt install ffmpeg -y
 apt install mpg123 -y
 npm install --global kill-port
