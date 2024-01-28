4d40d4599a33a8e6f5a8ca505e608785acb3909e
Phạm Thành Tuyên <149477072+phm-tuyenn@users.noreply.github.com>
2024-01-28 10:02:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source .bashrc
 nvm install 20.9
 sudo apt install --fix-broken
 sudo apt install ffmpeg -y
